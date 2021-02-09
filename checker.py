849327960d50b5a34ba8f909706cd840eeb8ca92
AMiWR <amiwrpremium@gmail.com>
2021-02-09 04:31:50 +0330

    optimized checker a bit, still not good

--- checker.py
+++ checker.py
@@ -9,14 +9,6 @@ path = str(input('Path To Proxy File To Check: '))
 system('clear')
 
 
-def get_list_proxy():
-    url = "https://api.proxyscrape.com/?request=getproxies&" \
-          "proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all"
-    r = requests.get(url)
-
-    return r.text, r.status_code
-
-
 def open_file():
     with open(path, 'r') as f:
         text = f.read()
@@ -37,14 +29,17 @@ class MainApp(threading.Thread):
         self.line_split = nice
         self.file_path = None
 
-        self.total = 0
-        self.valid = 0
-        self.invalid = 0
-
-        self.run()
-
     def run(self):
         if self.text:
+
+            global total_checked
+            global valid
+            global invalid
+
+            total_checked = 0
+            valid = 0
+            invalid = 0
+
             for i in range(len(self.line_split)):
                 try:
                     current = self.line_split.pop(i)
@@ -62,55 +57,69 @@ class MainApp(threading.Thread):
 
                         print(f'\033[92m[{x}]\t ~> \tProxy valid!')
 
-                        f = open('proxies.txt', 'a')
+                        f = open('good.txt', 'a')
                         f.write(f"\n{x}")
                         f.close()
 
-                        self.valid += 1
+                        valid += 1
+                        total_checked += 1
 
                     except requests.exceptions.ConnectTimeout:
                         print(f'\033[91m[{x}]\t ~> \tConnect timeout!')
-                        self.invalid += 1
+                        invalid += 1
+                        total_checked += 1
                         continue
                     except requests.exceptions.ConnectionError:
                         print(f'\033[91m[{x}]\t ~> \tConnection error!')
-                        self.invalid += 1
+                        invalid += 1
+                        total_checked += 1
                         continue
                     except requests.exceptions.HTTPError:
                         print(f'\033[91m[{x}]\t ~> \tHTTP error!')
-                        self.invalid += 1
+                        invalid += 1
+                        total_checked += 1
                         continue
                     except requests.exceptions.Timeout:
                         print(f'\033[91m[{x}]\t ~> \tTimeout error!')
-                        self.invalid += 1
+                        invalid += 1
+                        total_checked += 1
                         continue
                     except requests.exceptions.TooManyRedirects:
                         print(f'\033[91m[{x}]\t ~> \tToo many redirects!')
-                        self.invalid += 1
+                        invalid += 1
+                        total_checked += 1
                         continue
                     except Exception as e:
                         print(f'\033[91m[{x}]\t ~> \tUnknown error!\n{e}')
-                        self.invalid += 1
+                        invalid += 1
+                        total_checked += 1
                         continue
                 else:
                     print('No Proxy')
 
 
+def result():
+    print(f"\033[91m\n\nResult:\033[0m\n\n\n"
+          f"\033[35mChecked\t : \t{total_checked}\n"
+          f"\033[35mValid\t : \t{valid}\n"
+          f"\033[35mInvalids\t : \t{invalid}\n")
+
+
 if __name__ == '__main__':
-
-    global nice
-    nice = open_file()
-
-    threads = []
-    for z in range(client_thread):
-        threads.append(MainApp())
-    for thread in threads:
-        thread.daemon = True
-        thread.start()
-
-while True:
     try:
-        time.sleep(1)
+        global nice
+        nice = open_file()
+
+        threads = []
+        for z in range(client_thread):
+            threads.append(MainApp())
+        for thread in threads:
+            thread.daemon = True
+            thread.start()
+        for thread in threads:
+            thread.join()
+        result()
+
     except KeyboardInterrupt:
-        print(f"\033[0m\nExiting...\n\n\n")
-        exit(0)
+        print('\n\n\033[91mExiting...')
+        result()
